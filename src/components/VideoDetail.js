fa1c432bbc1ab33469fcc7c46606a84e46cbcea0
Daniel Torres <denial.torres@gmail.com>
2021-04-13 03:09:47 -0500

    Fix Warnings

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -2,23 +2,22 @@ import React from "react";
 import htmlReactParser from "html-react-parser";
 
 const VideoDetail = ({ video }) => {
-  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
-
   if (!video) {
     return <div>Loading..</div>;
-  } else {
-    return (
-      <div>
-        <div className="ui embed">
-          <iframe src={videoSrc} />
-        </div>
-        <div className="ui segment">
-          <h4 className="ui header">{htmlReactParser(video.snippet.title)}</h4>
-          <p>{htmlReactParser(video.snippet.description)}</p>
-        </div>
-      </div>
-    );
   }
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe title="video player" src={videoSrc} />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{htmlReactParser(video.snippet.title)}</h4>
+        <p>{htmlReactParser(video.snippet.description)}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -5,7 +5,11 @@ import htmlReactParser from "html-react-parser";
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img
+        alt={video.snippet.title}
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+      />
       <div className="content">
         <div className="header">{htmlReactParser(video.snippet.title)}</div>
       </div>
